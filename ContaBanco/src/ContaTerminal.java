0e5583d5c8593a95eca6531e514652d663692538
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2023-02-15 15:26:49 -0300

    Finalizando o projeto conta-banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println(" Por favor, digite o seu nome !");
+
+        String nome = leitor.nextLine();
+
+        System.out.println(" Por favor, digite o número da Agência !");
+
+        String agencia = leitor.nextLine();
+
+        System.out.println(" Por favor, digite o número da Conta !");
+
+        int numero = leitor.nextInt();
+
+        System.out.println(" Por favor, digite o saldo da Conta !");
+
+        double saldo = leitor.nextDouble();
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+
+        System.out.println( "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + df.format(saldo) + " já está disponível para saque.");
+
     }
 }
